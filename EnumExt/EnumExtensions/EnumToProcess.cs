df93c53320e587819e4fb36552eca9e9aa52b722
Boris Proshin <kaktusao@mail.ru>
2025-05-24 12:55:52 +0300

    Add LanguageExt library check

--- EnumExt/EnumExtensions/EnumToProcess.cs
+++ EnumExt/EnumExtensions/EnumToProcess.cs
@@ -4,13 +4,17 @@ using Microsoft.CodeAnalysis;
 
 namespace EnumExt.EnumExtensions;
 
-internal sealed record EnumToProcess(ITypeSymbol EnumSymbol, List<EnumMemberToProcess> Members,
-    string? FullNamespace)
+internal sealed record EnumToProcess(
+    ITypeSymbol EnumSymbol,
+    List<EnumMemberToProcess> Members,
+    string? FullNamespace,
+    bool HasLanguageExt)
 {
     public string FullCsharpName { get; } = EnumSymbol.ToDisplayString();
     public string DocumentationId { get; } = DocumentationCommentId.CreateDeclarationId(EnumSymbol);
-        
+
     public ITypeSymbol EnumSymbol { get; } = EnumSymbol;
     public List<EnumMemberToProcess> Members { get; } = Members;
     public string? FullNamespace { get; } = FullNamespace;
+    public bool HasLanguageExt { get; } = HasLanguageExt;
 }
